5b8c9b057a8e7fe2d312a2a6acba05e0fcc9786d
cchelton <59346941+cchelton@users.noreply.github.com>
2020-02-06 21:06:36 -0600

    initial commit

---
+++ test.js
@@ -0,0 +1,29 @@
+class Vehicle {
+  static count=0;   //<--Something like this
+  constructor() {
+    Vehicle.count++;
+  }
+}
+
+class Bike extends Vehicle {
+  static count=0;
+  constructor() {
+    super()
+    Bike.count++;
+  }
+}
+
+class Car extends Vehicle {
+  static count=0;
+  constructor() {
+    super()
+    Car.count++;
+  }
+}
+
+let softTail = new Bike();
+let hotRod = new Car();
+
+console.log(`Vehicles:${Vehicle.count}
+Bikes:${Bike.count}
+Cars:${Car.count}`);
